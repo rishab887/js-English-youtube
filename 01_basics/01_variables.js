6725882718ace7355a1eea1d07e55f2f906cd70b
Rishabh Gautam <rishabhgautam668@gmail.com>
2026-08-16 09:57:00 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 13334
+let accountName = "Rishabh"
+var accountAdd = "Delhi"
+accountPass = 2234
+/* 
+  never use var why ?
+  issue with block Scope and functional Scope
+*/
+// accountId = 54533 // not allowed as it is const
+console.table([accountId ,accountName , accountAdd , accountPass])
