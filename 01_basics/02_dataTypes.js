c8ac5021a45b08d60a829a6d44035a767a3374f0
Rishabh Gautam <rishabhgautam668@gmail.com>
2026-08-16 09:56:26 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+//"user strict" // treat all js code as newer version
+
+//alert( ) no use we are using node.js not browser
+
+let val;
+console.log(val);
+
+let num = null;
+console.log(num);
+
+/* 
+number => range is 2 to the power 56
+big int 
+string = ""
+null => standalone value
+boolean = true or false
+undefined
+Symbol
+object
+*/
+
+console.log(typeof val)
+console.log(typeof num)
+console.log(typeof "rishabh")
+console.log(typeof 0.34)
