3e23999c20404673ce66d36405e727d72aa773a1
Rishabh Gautam <rishabhgautam668@gmail.com>
2026-08-16 10:29:50 +0000

    Conversion and operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// let score = "33"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let value = Number(score)
+// console.log(typeof value);
+// console.log(value);//gives normal 33
+
+// let score2 = "33aba"
+// let value2 = Number(score2)
+// console.log(value2)//NaN not a number
+
+// let score3 = null
+// let value3 = Number(score3)
+// console.log(value3)//0
+
+// let score4 = true
+// let value4 = Number(score4)
+// console.log(value4)//if false = 0;
+
+// let score5 = "rishab"
+// let value5 = Number(score5)
+// console.log(value5)//NaN not a number
+
+let isloggedIN = "rishabh"
+let booleanIsloggesIn = Boolean(isloggedIN)
+// console.log(booleanIsloggesIn)
+// if 1 = true / 0 = false 
+// if "" = false;
+// if "rishab" = true;
+
+let stringnumber = 33
+let newString = String(stringnumber)
+console.log(newString)
+console.log(typeof newString)
