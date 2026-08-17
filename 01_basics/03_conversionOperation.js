0ca771adbb579f7cd5d6026e35f0fa5b8728e21d
Rishabh Gautam <rishabhgautam668@gmail.com>
2026-08-17 18:50:04 +0000

    Conversion Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -49,15 +49,3 @@ let st3 = st1 + st2;
 // console.log(true) // give true
 // console.log(+true) //gives 1
 // console.log(+"")//gives 0
-
-// console.log(null > 0)//false
-// console.log(null >= 0)//true
-// /*as above are the comparsion operator hence they convert the null to a value as 0 and then compare 
-// it so 0 > 0 false and 0 >= 0 true;
-// */
-
-// console.log(null == 0) // this is not a comparison operator so it shows false;
-
-console.log(undefined > 0)
-console.log(undefined < 0)
-console.log(undefined == 0)
