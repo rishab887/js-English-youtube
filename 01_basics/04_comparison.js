e08c569621e7685acd574de24334f1cb31dc6ce1
Rishabh Gautam <rishabhgautam668@gmail.com>
2026-08-17 18:45:06 +0000

    comparisons operatin

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+// console.log(null > 0)//false
+// console.log(null >= 0)//true
+// /*as above are the comparsion operator hence they convert the null to a value as 0 and then compare 
+// it so 0 > 0 false and 0 >= 0 true;
+// */
+
+// console.log(null == 0) // this is not a comparison operator so it shows false;
+
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined == 0)
